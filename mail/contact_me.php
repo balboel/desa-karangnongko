2ded69f4df2548f712a43f191a4f31836aa8a5f5
balboel <iqbalr346@gmail.com>
2025-01-30 23:36:18 +0700

    update contact

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,11 +17,26 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'desakarangnongko2018@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'belajarmoskov@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to, $email_subject, $email_body, $headers);
+$email_body = "You have received a new message from your website contact form.
+
+Here are the details:
+Name: $name
+Email: $email_address
+Phone: $phone
+Message:
+$message";
+
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+$headers .= "From: noreply@yourdomain.com\r\n";
+$headers .= "Reply-To: $email_address\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+
+if (mail($to, $email_subject, $email_body, $headers)) {
+   echo "Message sent successfully";
+} else {
+   echo "Message could not be sent";
+}
 return true;
-?>
